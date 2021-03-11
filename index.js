4ea915ddaea2e20af4e12d1d6afaafa46dbc41e2
Maria0303 <md2294481@gmail.com>
2021-03-10 19:47:34 -0500

    se agregaron funciones a index.js para mostrar y ocultar containerRules cuando se oprima el boton de rulers

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-function SaveAndHide(circulos) {
-    circulos.style.display = "none";
-  }
+function SaveAndHide(esconder){
+  esconder.style.display = "none";
 
-function SaveAndHide(containerRules) {
-    containerRules.style.display = "none";
-  }
+}
+
+function show(){
+  const mostrarContainerRules = document.getElementById("containerRules");
+  mostrarContainerRules.style.display = "block";
+  console.log (mostrarContainerRules);
+} 
+
+function init(){
+  const buscarContainerRules = document.getElementById("containerRules");
+  buscarContainerRules.style.display = "none";
+
+}
+
+init();
